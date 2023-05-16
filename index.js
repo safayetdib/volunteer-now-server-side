5925b2466cbbc5921cb2025e831821dd1261342a
safayet <safayetdib@gmail.com>
2023-05-16 23:01:52 +0600

    added and deployed to vercel

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-console.log(process.env);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +8,39 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dc2rluc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// await client.connect();
+
+		const eventsCollection = client.db('volunteerNow').collection('events');
+
+		app.get('/events', async (req, res) => {
+			const result = await eventsCollection.find().toArray();
+			res.send(result);
+		});
+
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send('Volunteer Now! - Give your time, change a life..');
